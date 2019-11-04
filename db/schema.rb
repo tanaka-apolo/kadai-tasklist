dcd06fe1592a39b893bc955bbc5b0b250ed25ef6
tanaka.apolo <tanaka.apolo1.15@gmail.com>
2019-11-04 06:01:29 +0000

    kadai-column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_070516) do
+ActiveRecord::Schema.define(version: 2019_11_03_033655) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
